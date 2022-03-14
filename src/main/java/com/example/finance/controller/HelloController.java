75911623ee4b64bb80bc5b43caeaff3ff6afef12
wsh <1131144533@qq.com>
2022-03-14 16:24:19 +0800

    //

---
+++ src/main/java/com/example/finance/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.finance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/index")
+    public String sayHello(){
+        return "hello";
+    }
+
+}
